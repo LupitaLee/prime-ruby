3b3450fdb6a3dcdb8a2a90c1366f6ca170db0dc5
Lupita <lupitarivera8899@gmail.com>
2021-02-04 20:48:16 -0600

    commit tree eabf31dac713af087e53ff241df797349969aaca

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+# require"pry"
+def prime?(num)
+    if num <= 1 
+        return false 
+    end
+    n = (2..(num -1)).to_a #sintax makes it to an array
+    # binding.pry
+    n.each do |value|
+    if num % value == 0 #num / to number not prime 
+    return false
+    end
+end
+true
+end
+
+
